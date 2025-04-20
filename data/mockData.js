43ca0e12f8b45d05552c29a065146f933c8867d1
nishant9933 <nishant9933@users.noreply.github.com>
2025-04-20 23:24:28 +0530

    Add data/mockData.js 1745171668311

---
+++ data/mockData.js
@@ -0,0 +1,23 @@
+export const services = [
+  {
+    id: '1',
+    title: 'Home Cleaning',
+    description: 'Professional home cleaning services for a spotless living space.',
+    price: 50,
+    image: 'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
+  },
+  {
+    id: '2',
+    title: 'Plumbing',
+    description: 'Expert plumbing services for all your repair and installation needs.',
+    price: 70,
+    image: 'https://images.pexels.com/photos/545053/pexels-photo-545053.jpeg',
+  },
+  {
+    id: '3',
+    title: 'Electrician',
+    description: 'Certified electricians for safe and reliable electrical work.',
+    price: 60,
+    image: 'https://images.pexels.com/photos/209235/pexels-photo-209235.jpeg',
+  },
+];
